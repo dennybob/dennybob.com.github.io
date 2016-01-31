8c955e3f64404ccffa40e952e63d30b9273e269b
dennybob <dennis.voss79@gmail.com>
2016-01-30 16:07:04 -0800

    r40a
    new calculator site - test

---
+++ shaft_volume/script.js
@@ -0,0 +1,13 @@
+function fix(fNumber,fPlace){
+  var fDecimal = Math.pow(10,fPlace);
+  return Math.round(fNumber*fDecimal)/fDecimal;
+  }
+
+function shaftVolumeWeight() {
+  var diameter = Number(document.getElementById("diameter").value);
+  var radius = diameter/2;
+  var length = Number(document.getElementById("length").value);
+  var answer = Math.PI * Math.pow(radius,2) * length;
+  answer = fix(answer,5);
+  document.getElementById("volume").value = answer;
+}
